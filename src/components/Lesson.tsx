6aa37fde1ee5085b49bafdc857b94f999b71f1d2
Jessica Niide <jessica.niide@gmail.com>
2022-06-24 21:31:10 -0300

    ignite lab app completed

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,52 @@
+import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { Link, useParams } from 'react-router-dom';
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>();
+  const isAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {locale: ptBR});
+  const isActiveLesson = slug === props.slug;
+  
+  return (
+    <Link to={`/event/lesson/${props.slug}`} className={`group ${isAvailable ? '' : 'pointer-events-none'}`}>
+      <span className="text-gray-300">
+        {availableDateFormatted}
+      </span>
+      <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : ''}`}
+      >
+        <header className="flex items-center justify-between">
+          {isAvailable
+          ? (
+          <span className={`flex gap-2 text-sm font-medium ${isActiveLesson ? 'text-white' : 'text-blue-500'}`}>
+            <CheckCircle size={20}/>
+            Conteúdo liberado
+          </span>
+          )
+          : 
+          (
+            <span className="flex gap-2 text-sm text-orange-500 font-medium">
+            <Lock size={20}/>
+            Em breve
+          </span>
+          )
+          }
+          <span className={`text-xs rounded px-2 py-[0.125rem] text-white border ${isActiveLesson ? 'border-white' : 'border-green-300'} font-bold uppercase`}>
+            {props.type === 'live' ? 'AO VIVO' : 'Aula prática' }
+          </span>
+        </header>
+        <strong className={`${isActiveLesson ? 'text-white' : 'text-gray-200'} mt-5 block`}>
+        {props.title}
+        </strong>
+      </div>
+    </Link>
+  )
+}
